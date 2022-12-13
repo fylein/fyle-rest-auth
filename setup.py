738e66f1a11571d4626522326ffa44dafc8679fe
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2022-12-13 15:22:42 +0530

    Handle Role restriction for Partner Dashboard (#26)
    * Handle Role restriction for Partner Dashboard
    
    * Update condition
    
    * fix pylint

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='fyle-rest-auth',
-    version='1.1.0',
+    version='1.2.0',
     author='Shwetabh Kumar',
     author_email='shwetabh.kumar@fyle.in',
     description='Django application to implement OAuth 2.0 using Fyle in Django rest framework',
